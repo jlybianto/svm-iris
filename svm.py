75aef38439e2674b941261ce19e9ebfbc984e175
jlybianto <jesse.lybianto@gmail.com>
2016-03-16 23:15:11 -0600

    Add visualization of dataset

--- svm.py
+++ svm.py
@@ -6,3 +6,29 @@ from sklearn import datasets, svm
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
+
+# ----------------
+# OBTAIN DATA
+# ----------------
+
+iris = datasets.load_iris()
+
+# ----------------
+# VISUALIZE DATA
+# ----------------
+
+plt.scatter(iris.data[:, 1], iris.data[:, 2], c=iris.target)
+plt.xlabel(iris.feature_names[1])
+plt.ylabel(iris.feature_names[2])
+plt.title("Iris Dataset - Setosa, Versicolor and Virginica")
+plt.show()
+
+plt.scatter(iris.data[0:100, 1], iris.data[0:100, 2], c=iris.target[0:100])
+plt.xlabel(iris.feature_names[1])
+plt.ylabel(iris.feature_names[2])
+plt.title("Iris Dataset - Setosa and Versicolor")
+plt.show()
+
+# ----------------
+# MODEL DATA
+# ----------------
