548e82fb531bfe95eea24a01979e7c1e69d9b76e
jlybianto <jesse.lybianto@gmail.com>
2016-03-16 22:42:23 -0600

    Import initial packages

---
+++ svm.py
@@ -0,0 +1,8 @@
+# ----------------
+# IMPORT PACKAGES
+# ----------------
+
+from sklearn import datasets, svm
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
